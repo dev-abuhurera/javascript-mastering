45f21f9ec94573cda803fe8402094908d587fdd5
M Abuhurera <abuhurerarohani@gmail.com>
2025-09-30 20:02:06 +0000

    "Javascript datatypes"

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 144553;
+let accountEmail = "example@gmail.com";
+var accountPassword = "1234";
+accountCity = "layyah";
+let accountState;
+
+// accountId = 2; // not allowed to change 
+
+accountEmail = "Hrf@gmail.com"
+accountPassword = "214421"
+accountCity = "islamabad"
+
+
+
+console.log(accountId);
+
+
+/*
+    Not gonna use var because it has scope problem in the block and function
+*/
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState]);
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+
+let name = "hurera" // string 
+let age = 14 // number
+let isLoggedIn = true // boolean
+let state = null // Empty
+
+// primitive 
+
+/* 
+
+number, string, boolean, undefined, null(datatype + standalone value)
+undefined => when no value is defined 
+null => representation of an empty value
+symbol
+
+*/
+
+//Non primitives
+
+/*
+
+Array -- Object 
+
+*/
+console.log(typeof age);
+console.log(typeof null); // is an object
+console.log(typeof undefined);
+
+
+
