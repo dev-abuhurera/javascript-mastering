76d4fb9f86563e2bee08ba5d1f6469b6119bc518
M Abuhurera <abuhurerarohani@gmail.com>
2025-10-05 13:18:05 +0000

    comparethedatatypes

---
+++ 01_basics/04_datatypeComparison.js
@@ -0,0 +1,32 @@
+console.log("2" > 1); 
+
+// String -> number
+
+// String -> number
+
+console.log(typeof "2" > 1); 
+
+// js -> string -> number but string -> NaN --->>> 
+
+// NaN > number -->>> false
+
+// comparison ---- equality check
+
+
+// == is start converting the data types
+
+console.log("2" == 2);
+
+// === is the strict check that will also check the datatypes and all
+
+console.log("2" === 2); // no conversions
+
+// ---------------------------------------------------------------------
+
+
+
+
+
+
+
+
