4565f6d3792b049bd0818448c44dabba8ae7bd6d
M Abuhurera <abuhurerarohani@gmail.com>
2025-10-05 19:27:51 +0000

    used the math and numbers in js

---
+++ 01_basics/07_Number_and_Math.js
@@ -0,0 +1,40 @@
+const score = 400; // Number defination
+console.log(score)
+// toString
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+
+// This convert the number to the string and then all string methods can be used 
+
+const hundreds = 1000000;
+
+console.log(hundreds.toLocaleString());
+
+
+const otherNumber = 123.8966
+
+console.log(otherNumber.toPrecision(4));
+
+//-------------------------------------------------------------
+
+// -------------------------Maths----------------------
+
+console.log(Math);
+
+console.log(Math.abs(-4));
+
+console.log(Math.round(4.6));
+
+console.log(Math.ceil(-4));
+
+
+console.log(Math.random()); // 0.00 -> 1 values
+console.log(Math.random() * 10);
+
+
+//--------------------------------------------------------------------------
+
+
