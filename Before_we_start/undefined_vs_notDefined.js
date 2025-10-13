cac78dd6d49ff6592d174f9649711112bb466934
M Abuhurera <abuhurerarohani@gmail.com>
2025-10-13 04:01:23 +0000

    "undefine vs not define"

---
+++ Before_we_start/undefined_vs_notDefined.js
@@ -0,0 +1,25 @@
+// Javascript is a loosly typed language --->> it means it assign and detect the datatypes of the variables by themselve
+
+
+var a;
+
+console.log(a);
+
+
+if(a === undefined){
+    console.log("a is undefined")
+}
+else{
+    console.log("a is not undefined")
+}
+
+
+console.log(x)
+
+var x; 
+
+x = 10
+x = "Hello js"
+
+
+/////////////////////////////////////////////////////////////////////////////////
