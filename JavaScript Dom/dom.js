62b82a1461288331fdd808e80eefd434f7ff9069
M Abuhurera <abuhurerarohani@gmail.com>
2026-02-09 21:29:21 +0000

    "Dom-Begins"

---
+++ JavaScript Dom/dom.js
@@ -0,0 +1,40 @@
+// What is DOM 
+// Javascript used in the front end
+
+/*
+
+    - Js way of controlling the HTML
+    
+    ------> How the Dom is created 
+
+    1. HTML ----> (static Page) ----> Browser Reads it ----> Creates DOM ----> js can control it 
+     
+
+*/
+
+
+// 4 Pillars of DOM 
+
+/*
+
+    - Selection of an Element ---- (1)
+    - Changing HTML ---- (2)  
+    - Changing CSS ---- (3)
+    - Event Listener ---- (4)
+
+*/
+
+
+// Selection of an Element 
+
+const heading = document.querySelector("h1");
+console.log(heading);
+
+heading.innerHTML = "Changed HTML"
+heading.style.color = "red"
+
+// Event Listeners 
+
+heading.addEventListener('click', function(){
+    alert("I am clicked!!!!")
+})
