204282d5e6bb04fe155b304d0b5a240931034378
M Abuhurera <abuhurerarohani@gmail.com>
2026-02-20 17:22:09 +0000

    "intro to object"

--- Javascript Programs/19_intro_object.js
+++ Javascript Programs/19_intro_object.js
@@ -1,4 +1,27 @@
 // Introduction to object
 
+const jonasArray = [
+
+    'Jonas',
+    'scherodinger' , 
+    2037 - 1991 , 
+    'teacher' ,
+    ['michel' , 'peter' , 'steven']
+
+] // Array ====> not giving the element by it's name 
+
+// Objects is the key value pair 
+
+const jonas = {
+
+    firstName : 'Jones',
+    lastName : 'Scherodinger', // Each of the key is called as the property
+    age : 2045 - 1991,
+    job : 'teacher',
+    friends :  ['michel' , 'peter' , 'steven']
+
+}
+
+
 
 
