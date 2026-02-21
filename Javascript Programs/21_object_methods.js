b8883352ea9afd9e698487ab6120adc2befbd4fe
M Abuhurera <abuhurerarohani@gmail.com>
2026-02-21 01:31:14 +0000

    object_methods

--- Javascript Programs/21_object_methods.js
+++ Javascript Programs/21_object_methods.js
@@ -7,7 +7,7 @@ const jonas = {
 
     firstName : 'Jones',
     lastName : 'Scherodinger', // Each of the key is called as the property
-    age : 2045 - 1991,
+    // age : 2045 - 1991,
     birthYear : 1991,
     job : 'teacher',
     friends :  ['michel' , 'peter' , 'steven'],
@@ -17,12 +17,23 @@ const jonas = {
     //     return 2037 - birthYear;
     // } // function attached to any object is a method
 
-    calAge : function(){ // Function expression
-        console.log(this)
-        return 2037 - this.birthYear;
-    } // function attached to any object is a method
+    // calAge : function(){ // Function expression
+    //     console.log(this)
+    //     return 2037 - this.birthYear;
+    // } // function attached to any object is a method
     
+    calAge : function(){ // Function expression
+        this.age = 2037 - this.birthYear; // we are creating a new property to the object
+        // return this.age;
+    }, // function attached to any object is a method
 
+    getSummary : function(){
+
+    return `${this.firstName} is the firstName and ${this.lastName} is the lastName and the birthYear is ${this.birthYear}, with job of ${this.job} and the friends Name are: ${this.friends[0] + ' ' + this.friends[1] + ' ' + this.friends[2]} and has ${this.hasDriverData ? 'a' : 'no'}  driver License`;
+
+    }
+
+   
 }
 
 
@@ -36,3 +47,19 @@ const jonas = {
 
 
 console.log(jonas.calAge());
+
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+
+console.log(jonas.getSummary())
+
+
+// Important
+
+
+// Arrays are also special type of objects
+
+
+
+
