10a362ba473f4dc1e7cbcebc2f4577c35fd00524
M Abuhurera <abuhurerarohani@gmail.com>
2026-02-26 18:05:57 +0000

    modelWindow Project

--- Projects/ModalWindow/script.js
+++ Projects/ModalWindow/script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal'); // only the first one selected
+
+console.log(btnCloseModal);
+console.log(btnOpenModal); 
+
+for(let i = 0; i < btnOpenModal.length; i++){
+    btnOpenModal[i].addEventListener('click', () => {
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+}
