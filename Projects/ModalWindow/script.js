0a23fbc1b337273b2be639055016e8921382bdc4
M Abuhurera <abuhurerarohani@gmail.com>
2026-02-26 18:12:02 +0000

    added the hiding functionality

--- Projects/ModalWindow/script.js
+++ Projects/ModalWindow/script.js
@@ -9,8 +9,21 @@ console.log(btnCloseModal);
 console.log(btnOpenModal); 
 
 for(let i = 0; i < btnOpenModal.length; i++){
+
     btnOpenModal[i].addEventListener('click', () => {
         modal.classList.remove('hidden');
         overlay.classList.remove('hidden');
     });
+
+    const closeModal = () => {
+
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+
+    }
+
+    btnCloseModal.addEventListener('click', closeModal);
+
+    overlay.addEventListener('click', closeModal);
+
 }
